88636317cadbe1799e3e2fb9bf15a6bd5762d6b3
tamather <taygergix@gmail.com>
2019-10-15 14:19:25 +0200

    fix ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: tamather <tamather@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 11:33:48 by tamather          #+#    #+#             */
-/*   Updated: 2019/10/14 12:41:23 by tamather         ###   ########.fr       */
+/*   Updated: 2019/10/15 14:18:49 by tamather         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,19 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	size_t i;
 	size_t j;
-	size_t k;
 
 	i = 0;
-
-	while (s2 && i < n + 1)
+	if (s2[i] == '\0')
+		return ((char*)s1);
+	while (*s1 && i < n)
 	{
 		j = 0;
-		k = 0;
-		while (s1[k] == s2[j] && s1[k] && s2[j])
-		{
+		while (s2[j] == s1[j] && s2[j] && s1[j])
 			j++;
-			k++;
-		}
-		if (s1[k] == '\0' || i + k > n + 1)
-			return ((char*)s2);
-		s2++;
+		if (s2[j] == '\0' && i + j < n)
+			return ((char*)s1);
+		s1++;
+		i++;
 	}
-	return ((char*)s2);
+	return (0);
 }
