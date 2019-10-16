2921481575ff08dd787eb9da41237713b6ee7540
tayger <taygergix@gmail.com>
2019-10-16 07:29:34 +0200

    ft_move

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tamather <tamather@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/14 03:26:06 by tamather          #+#    #+#             */
-/*   Updated: 2019/10/15 15:14:38 by tamather         ###   ########.fr       */
+/*   Updated: 2019/10/16 05:20:22 by tamather         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 void            *ft_memchr(const void *s, int c, size_t n);
+void            *ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
